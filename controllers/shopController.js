c46ea488ef6289bc4238be78de3b333b67eb7e0c
kentng01 <57498837+kentng01@users.noreply.github.com>
2021-12-20 12:27:58 -0500

    class 12/20

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,6 +1,14 @@
+exports.middlewareSample = async (req,res,next)=>{
+    req.name= "TEST";
+    next();
+};
+
+
+
 exports.homePage = async (req,res)=>{
     const stores = ["Dunkin", "Tim Hortons", "STarbucks"];
     try{
+        console.log(req.name)
         res.json(stores)
     }catch (error) {
         console.log();
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = new express.Router(); //instantiate express router
 const shopController = require("../controllers/shopController");
 
-router.get("/", shopController.homePage);
+router.get("/", shopController.middlewareSample, shopController.homePage);
 
 module.exports = router;  //export file
