c07c3dec815c38d18d202f86ab041db28218f544
kentng01 <57498837+kentng01@users.noreply.github.com>
2022-01-14 12:26:36 -0500

    IT WORKS

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-require("dotenv").config({pth: "variables.env"});
+require("dotenv").config({path: "variables.env"});
 
 mongoose
     .connect(`${process.env.DATABASE}`,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log("Connected to DB"))
+}).then(() => console.log("Connected to DATABASE"));
 
 mongoose.connection.on("error", (err)=>{
     console.log(`${err.message}`);
