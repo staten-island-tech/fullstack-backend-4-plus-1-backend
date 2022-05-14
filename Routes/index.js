ef8ddf5a1fccfc18a554c7f18186838449ec117d
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2022-05-13 21:19:20 -0400

    :/

--- Routes/index.js
+++ Routes/index.js
@@ -5,23 +5,23 @@ const authController = require("../Controllers/authMiddleWare");
 
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
-const checkJwt = jwt({
-  // Dynamically provide a signing k`ey based on the kid in the header and the signing keys provided by the JWKS endpoint
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://dev-2szf794g.us.auth0.com/.well-known/jwks.json`,
-  }),
+// const checkJwt = jwt({
+//   // Dynamically provide a signing k`ey based on the kid in the header and the signing keys provided by the JWKS endpoint
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `https://dev-2szf794g.us.auth0.com/.well-known/jwks.json`,
+//   }),
 
-  // Validate the audience and the issuer
-  audience: "http://localhost:8080", //replace with your API's audience, available at Dashboard > APIs
-  issuer: "https://dev-2szf794g.us.auth0.com/",
-  algorithms: ["RS256"],
-});
+//   // Validate the audience and the issuer
+//   audience: "http://localhost:6000", //replace with your API's audience, available at Dashboard > APIs
+//   issuer: "https://dev-2szf794g.us.auth0.com/",
+//   algorithms: ["RS256"],
+// });
 
 router.get("/:id", leaderBoardController.getBeatmapData);
-router.get("/", checkJwt, leaderBoardController.getUser);
+router.get("/", leaderBoardController.getUser);
 
 router.get(
  "/auth",
