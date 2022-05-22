30d3b54daa9f924745b3580d4cc50855d6a713b7
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2022-05-22 00:28:00 -0400

    backend model update

--- Models/users.js
+++ Models/users.js
@@ -1,40 +1,104 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
-const UserSchema = new mongoose.Schema({
+const volSettingsSchema = mongoose.Schema({
+ master: {
+  type: String,
+  default: "",
+ },
+ music: {
+  type: String,
+  default: "",
+ },
+ hitSound: {
+  type: String,
+  default: "",
+ },
+});
+
+const userSettingsSchema = mongoose.Schema({
  username: {
   type: String,
   trim: true,
   required: "please enter a Username",
  },
  slug: String,
+ profilePicture: {
+  type: String,
+  default: "",
+ },
+});
+
+const gameSettingsSchema = mongoose.Schema({
+ scrollSpeed: {
+  type: String,
+  default: "",
+ },
+ keys: {
+  type: String,
+  default: "",
+ },
+ pauseKey: {
+  type: String,
+  default: "",
+ },
+ restartKey: {
+  type: String,
+  default: "",
+ },
+});
+
+const gameScoreDataSchema = mongoose.Schema({
  leaderBoardPos: {
   type: String,
   trim: true,
+  default: "",
  },
  playCount: {
   type: String,
   trim: true,
+  default: "",
  },
  accuracy: {
   type: String,
   trim: true,
+  default: "",
  },
  performance: {
   type: String,
   trim: true,
+  default: "",
  },
  SS: {
+  default: "",
   type: String,
   trim: true,
  },
  S: {
+  default: "",
   type: String,
   trim: true,
  },
  A: {
+  default: "",
   type: String,
   trim: true,
+ },
+});
+
+const UserSchema = new mongoose.Schema({
+ userSettings: {
+  type: userSettingsSchema,
+  //   default: {}
+ },
+ volSettings: {
+  type: volSettingsSchema,
+ },
+ gameSettings: {
+  type: gameSettingsSchema,
+ },
+ gameData: {
+  type: gameScoreDataSchema,
  },
 });
 
